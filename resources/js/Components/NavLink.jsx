c66053549e36fb5067896d061463139cf2ed03db
Pasquale Perfetto <pasquale.perf@gmail.com>
2022-12-15 15:07:53 +0100

    fix navbar utils links focus color, added cards in homepage

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -1,7 +1,6 @@
 import { usePage, Link } from '@inertiajs/inertia-react';
 
 export default function NavLink({ href, active, children, method }) {
-
     return (
         <Link
             method={method}
@@ -10,7 +9,7 @@ export default function NavLink({ href, active, children, method }) {
             className={
                 active
                     ? 'active inline-flex items-center mr-2 px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-                    : 'inline-flex items-center mr-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-white hover:text-gray-100 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+                    : 'inline-flex items-center mr-2 px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-white hover:text-gray-100 hover:border-gray-300 focus:outline-none focus:text-gray-100 focus:border-gray-300 transition duration-150 ease-in-out'
             }
         >
             {children}
--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -7,6 +7,22 @@ const Homepage = (props) => {
         <LayoutApp auth={props.auth}>
             <Head title="Homepage" />
             <h1>Homepage</h1>
+            <div className="cards-container min-h-screen flex flex-col md:flex-row align-center justify-center max-w-md space-y-9 md:space-y-0 md:space-x-6 mx-auto">
+                {/* Cards 1 */}
+                <div class="max-w-xs rounded overflow-hidden shadow-lg">
+                    <img class="w-full" src="https://picsum.photos/300/300" alt="Sunset in the mountains"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    </div>
+                </div>
+                {/* Cards 2 */}
+                <div class="max-w-xs rounded overflow-hidden shadow-lg">
+                    <img class="w-full" src="https://picsum.photos/300/300" alt="Sunset in the mountains"/>
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    </div>
+                </div>
+            </div>
         </LayoutApp>
 
        </>
